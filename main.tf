d32083caa339a822569d01d3255f27cd4a351338
Josiel Santos <josiel.lkp@gmail.com>
2019-08-31 00:16:18 -0300

    Criando máquina (droplet) com chave SSH

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "digitalocean" {
     version = "~> 1.7"
 }
+
+resource "digitalocean_ssh_key" "default" {
+    name = "Terraform"
+    public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "digitalocean_droplet" "web" {
+    name = "web"
+    region = "nyc1"
+    size = "s-1vcpu-1gb"
+    image = "ubuntu-18-04-x64"
+    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
+}
