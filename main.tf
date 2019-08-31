78a8748badbea483bb5d12cfbae83f7504a93614
Josiel Santos <josiel.lkp@gmail.com>
2019-08-31 00:36:24 -0300

    Imprimir o endereço IP da máquina ao rodar 'terraform apply'

--- main.tf
+++ main.tf
@@ -14,3 +14,7 @@ resource "digitalocean_droplet" "web" {
     image = "ubuntu-18-04-x64"
     ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
 }
+
+output "ipv4_address" {
+    value = "${digitalocean_droplet.web.ipv4_address}"
+}
