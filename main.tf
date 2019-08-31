ea06e61d3bccffa6a39da05254f15f35175b8eb2
Josiel Santos <josiel.lkp@gmail.com>
2019-08-31 00:47:56 -0300

    main.tf reescrito para a versão 0.12 do terraform, adicionado arquivo versions.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 provider "digitalocean" {
-    version = "~> 1.7"
+  version = "~> 1.7"
 }
 
 resource "digitalocean_ssh_key" "default" {
-    name = "Terraform"
-    public_key = "${file("~/.ssh/id_rsa.pub")}"
+  name       = "Terraform"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "digitalocean_droplet" "web" {
-    name = "web"
-    region = "nyc1"
-    size = "s-1vcpu-1gb"
-    image = "ubuntu-18-04-x64"
-    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
+  name     = "web"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-18-04-x64"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 }
 
 output "ipv4_address" {
-    value = "${digitalocean_droplet.web.ipv4_address}"
+  value = digitalocean_droplet.web.ipv4_address
 }
+
