905d1515f96509831ef1c98169f61130aa772b29
Josiel Santos <josiel.lkp@gmail.com>
2019-09-01 00:15:19 -0300

    Provisionando droplets, testando o load balancer

--- droplet.tf
+++ droplet.tf
@@ -5,4 +5,21 @@ resource "digitalocean_droplet" "web" {
   size     = "s-1vcpu-1gb"
   image    = "ubuntu-18-04-x64"
   ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt update",
+      "apt install -y apache2",
+      "echo server-${count.index}>/var/www/html/index.html",
+      "chown www-data:www-data /var/www/html/index.html",
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.ipv4_address
+      user        = "root"
+      private_key = file("~/.ssh/id_rsa")
+      timeout     = "2m"
+    }
+  }
 }
